3e8532e0c679b46058d6de6a7dd5fe83e381813c
alessandra <alessandraamontanher@gmail.com>
2020-09-17 11:20:04 -0300

    console refactored

--- BehavioralDesignPatters/Presentation/Program.cs
+++ BehavioralDesignPatters/Presentation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Pattern.ChainOfResponsability;
+using System;
 
 namespace Presentation
 {
@@ -6,8 +7,38 @@ namespace Presentation
     {
         static void Main(string[] args)
         {
-            var chain = new ChainDemo();
-            chain.Run();
+            InitMessage();
+
+            Chain();
         }
+
+        #region Console Props
+        private static void InitMessage()
+        {
+            var message = "BEHAVIORAL DESIGN PATTERNS - GENERIC REPOSITORY";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        static void ConsoleColorMethod(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Environment.NewLine + text + Environment.NewLine);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        #endregion
+
+        #region Invoke Patterns        
+
+        private static void Chain()
+        {
+            ConsoleColorMethod("### [START] - CHAIN OF RESPONSABILITY ###");
+
+            new ChainDemo().Run();
+
+            ConsoleColorMethod("### [END] - CHAIN OF RESPONSABILITY ###");
+        }
+
+        #endregion
     }
 }
