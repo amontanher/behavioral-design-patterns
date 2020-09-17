0f2243371a6e73bdd8d764ea1a5a662003c62447
alessandra <alessandraamontanher@gmail.com>
2020-09-17 10:54:01 -0300

    chain refactored

--- BehavioralDesignPatters/Pattern.ChainOfResponsability/ChainDemo.cs
+++ BehavioralDesignPatters/Pattern.ChainOfResponsability/ChainDemo.cs
@@ -1,10 +1,49 @@
-﻿namespace Pattern.ChainOfResponsability
+﻿using Pattern.ChainOfResponsability.Pattern.Domain;
+using System;
+
+namespace Pattern.ChainOfResponsability
 {
     public class ChainDemo
     {
+        HiringProcess hr;
+        HiringProcess doctor;
+        HiringProcess manager;
+
+        /// <summary>
+        /// This demo is about a simple HR Process to hire an employee
+        /// </summary>
         public void Run()
         {
+            Console.WriteLine("Starting ChainOfResponsability");
 
+            SetupChainOfResponsabilities();
+            SetupChainSucessors();
+
+            ProcessStep[] hiringProcess = {
+                ProcessStep.SELECTION,
+                ProcessStep.INTERVIEW,
+                ProcessStep.EXAMS,
+                ProcessStep.FINISHED
+            };
+
+            foreach (var process in hiringProcess)
+                hr.HandleProcess(process);
+
+            Console.WriteLine("Finishing ChainOfResponsability");
+        }
+
+        private void SetupChainOfResponsabilities()
+        {
+            hr = new HRAnalyst();
+            manager = new Manager();
+            doctor = new Doctor();
+        }
+
+        private void SetupChainSucessors()
+        {
+            hr.SetSucessor(manager);
+            manager.SetSucessor(doctor);
+            doctor.SetSucessor(hr);
         }
     }
 }
---
+++ BehavioralDesignPatters/Pattern.ChainOfResponsability/Pattern.Domain/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Pattern.ChainOfResponsability.Pattern.Domain
+{
+    public class Doctor : HiringProcess
+    {
+        public override void HandleProcess(ProcessStep step)
+        {
+            if (ProcessStep.EXAMS.Equals(step))
+            {
+                Console.WriteLine("---> Doctor finished exams.");
+                Thread.Sleep(1000);
+            }
+            else
+                _sucessor.HandleProcess(step);
+        }
+    }
+}
